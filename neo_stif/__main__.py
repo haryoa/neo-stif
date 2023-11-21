9782acf40d6f0871590cb1c44931c4f95ae67287
haryoa <haryo97@gmail.com>
2023-11-21 22:10:29 +0400

    feat: add other feats

--- neo_stif/__main__.py
+++ neo_stif/__main__.py
@@ -1,52 +1,282 @@
 import fire
-from transformers import AutoTokenizer, BertForTokenClassification, BertConfig
+from transformers import AutoTokenizer, BertForTokenClassification, BertConfig, BertForMaskedLM
 from neo_stif.components.utils import create_label_map
 import pandas as pd
 from neo_stif.components.train_data_preparation import prepare_data_tagging_and_pointer
 import datasets
-from neo_stif.lit import LitTaggerOrInsertion
+from neo_stif.lit import LitPointer, LitTaggerOrInsertion
 from torch.utils.data import DataLoader
-from neo_stif.components.collator import FelixCollator
+from neo_stif.components.collator import FelixCollator, FelixInsertionCollator
 from lightning import Trainer
-from lightning.pytorch.callbacks import RichProgressBar
+from lightning.pytorch.callbacks import RichProgressBar, ModelCheckpoint, EarlyStopping
 from neo_stif.components.utils import compute_class_weights
+from datasets import load_from_disk
 
 
-MAX_MASK = 35
+MAX_MASK = 30
 USE_POINTING = True
 
 
-model_dict = {
-    'koto': "indolem/indobert-base-uncased"
-}
+model_dict = {"koto": "indolem/indobert-base-uncased"}
 
-def train(part: str = 'tagger', model='koto', batch_size=32, 
-          with_validation=False, do_compute_class_weight=False):
+
+LR_TAGGER = 2e-5 # due to the pre-trained nature
+LR_POINTER = 1e-3 # no pre-trained
+LR_INSERTION = 2e-5 # due to the pre-trained nature
+VAL_CHECK_INTERVAL = 20
+
+
+def insertion(
+    processed_train_data,
+    processed_dev_data,
+    tokenizer,
+    batch_size,
+    model_path_or_name,
+    device="cuda",
+    label_dict=None,
+):
+    """
+    Perform insertion task using the given parameters.
+
+    Args:
+        processed_train_data (str): Filepath to the processed training data.
+        processed_dev_data (str): Filepath to the processed development data.
+        tokenizer: Tokenizer object used for tokenizing the data.
+        batch_size (int): Number of samples per batch.
+        model_path_or_name (str): Path or name of the pre-trained model.
+        device (str, optional): Device to use for training. Defaults to "cuda".
+        label_dict (dict, optional): Dictionary mapping labels to their indices. Defaults to None.
+
+    Returns:
+        None
+    """
+    
+    rich_cb = RichProgressBar()
+    checkpoint_callback = ModelCheckpoint(
+        dirpath="output/stif-i-f/felix-insert/",
+        filename="{epoch}-{val_loss:.2f}-{f1_val_step:.2f}",
+        save_top_k=2,
+        monitor="val_loss",
+        mode="min",
+    )
+    ea_stop = EarlyStopping(patience=5, monitor="val_loss", mode="min")
+    train_data = load_from_disk(processed_train_data)
+    dev_data = load_from_disk(processed_dev_data)
+    train_dl = DataLoader(
+        train_data,
+        batch_size=batch_size,
+        shuffle=True,
+        collate_fn=FelixInsertionCollator(tokenizer, pad_label_as_input=len(label_dict)),
+    )
+    dev_dl = DataLoader(
+        dev_data,
+        batch_size=batch_size,
+        shuffle=True,
+        collate_fn=FelixCollator(tokenizer, pad_label_as_input=len(label_dict)),
+    )
+    model = BertForMaskedLM.from_pretrained(model_path_or_name)
+    lit_insert = LitTaggerOrInsertion(
+        model,
+        lr=LR_INSERTION,
+        num_classes=len(label_dict),
+        class_weight=None,
+        tokenizer=tokenizer,
+        label_dict=label_dict,
+    )
+    trainer = Trainer(
+        accelerator=device,
+        devices=1,
+        val_check_interval=20,
+        check_val_every_n_epoch=None,
+        callbacks=[rich_cb, checkpoint_callback, ea_stop],
+    )
+    trainer.fit(lit_insert, train_dl, dev_dl)
+
+
+
+
+def tagger(
+    df_train,
+    data_train,
+    tokenizer,
+    batch_size,
+    model_path_or_name,
+    with_validation=False,
+    do_compute_class_weight=False,
+    device="cuda",
+    label_dict=None,
+):
+    rich_cb = RichProgressBar()
+
+    checkpoint_callback = ModelCheckpoint(
+        dirpath="output/stif-i-f/felix-tagger/",
+        filename="{epoch}-{val_loss:.2f}-{f1_val_step:.2f}",
+        save_top_k=2,
+        monitor="val_loss",
+        mode="min",
+    )
+    ea_stop = EarlyStopping(patience=5, monitor="val_loss", mode="min")
+    dev_dl = None
+    class_weights = (
+        compute_class_weights(df_train.label.apply(eval), num_classes=len(label_dict))
+        if do_compute_class_weight
+        else None
+    )
+    print(class_weights)
+    if with_validation:
+        df_dev = pd.read_csv("data/stif_indo/dev_with_pointing.csv")
+        data_dev = datasets.Dataset.from_pandas(df_dev)
+        data_dev, label_dict = prepare_data_tagging_and_pointer(
+            data_dev, tokenizer, label_dict
+        )
+        dev_dl = DataLoader(
+            data_dev,
+            batch_size=batch_size,
+            shuffle=True,
+            collate_fn=FelixCollator(tokenizer, pad_label_as_input=len(label_dict)),
+        )
+
+    pre_trained_bert = BertForTokenClassification.from_pretrained(
+        model_path_or_name, num_labels=len(label_dict)
+    )
+    lit_tagger = LitTaggerOrInsertion(
+        pre_trained_bert,
+        lr=LR_TAGGER,
+        num_classes=len(label_dict),
+        class_weight=class_weights,
+        tokenizer=tokenizer,
+        label_dict=label_dict,
+    )
+    train_dl = DataLoader(
+        data_train,
+        batch_size=batch_size,
+        shuffle=True,
+        collate_fn=FelixCollator(tokenizer, pad_label_as_input=len(label_dict)),
+    )
+    trainer = Trainer(
+        accelerator=device,
+        devices=1,
+        val_check_interval=20,
+        check_val_every_n_epoch=None,
+        callbacks=[rich_cb, checkpoint_callback, ea_stop],
+    )
+    trainer.fit(lit_tagger, train_dl, dev_dl)
+
+
+def pointer(
+    data_train,
+    tokenizer,
+    batch_size,
+    with_validation=False,
+    device="cuda",
+    label_dict=None,
+):
+    rich_cb = RichProgressBar()
+    pointer_network_config = BertConfig(
+        vocab_size=len(label_dict) + 1,
+        num_hidden_layers=2,
+        num_attention_heads=1,
+        pad_token_id=len(label_dict),
+    )  # + 1 as the pad token
+    lit_pointer = LitPointer(
+        pointer_network_config,
+        lr=LR_POINTER,
+        num_classes=None
+    )
+
+    checkpoint_callback = ModelCheckpoint(
+        dirpath="output/stif-i-f/felix-pointer/",
+        filename="{epoch}-{val_loss:.2f}-{f1_val_step:.2f}",
+        save_top_k=2,
+        monitor="val_loss",
+        mode="min",
+    )
+    ea_stop = EarlyStopping(patience=5, monitor="val_loss", mode="min")
+    dev_dl = None
+
+    if with_validation:
+        df_dev = pd.read_csv("data/stif_indo/dev_with_pointing.csv")
+        data_dev = datasets.Dataset.from_pandas(df_dev)
+        data_dev, label_dict = prepare_data_tagging_and_pointer(
+            data_dev, tokenizer, label_dict
+        )
+        dev_dl = DataLoader(
+            data_dev,
+            batch_size=batch_size,
+            shuffle=True,
+            collate_fn=FelixCollator(tokenizer, pad_label_as_input=len(label_dict)),
+        )
+
+    train_dl = DataLoader(
+        data_train,
+        batch_size=batch_size,
+        shuffle=True,
+        collate_fn=FelixCollator(tokenizer, pad_label_as_input=len(label_dict)),
+    )
+    trainer = Trainer(
+        accelerator=device,
+        devices=1,
+        val_check_interval=20,
+        check_val_every_n_epoch=None,
+        callbacks=[rich_cb, checkpoint_callback, ea_stop],
+    )
+    trainer.fit(lit_pointer, train_dl, dev_dl)
+
+
+def train_stif(
+    part: str = "tagger",
+    model="koto",
+    batch_size=32,
+    with_validation=False,
+    do_compute_class_weight=False,
+    device="cuda",
+):
     tokenizer = AutoTokenizer.from_pretrained("indolem/indobert-base-uncased")
     label_dict = create_label_map(MAX_MASK, USE_POINTING)
-    rich_cb = RichProgressBar()
+
+    # Callback for trainer
 
     df_train = pd.read_csv("data/stif_indo/train_with_pointing.csv")
     data_train = datasets.Dataset.from_pandas(df_train)
-    data_train, label_dict = prepare_data_tagging_and_pointer(data_train, tokenizer, label_dict)
+    data_train, label_dict = prepare_data_tagging_and_pointer(
+        data_train, tokenizer, label_dict
+    )
     model_path_or_name = model_dict[model]
-    
-    if part == 'tagger':
-        dev_dl = None
-        class_weights = compute_class_weights(df_train.label.apply(eval), num_classes=len(label_dict)) if do_compute_class_weight else None
-        print(class_weights)
-        if with_validation:
-            df_dev = pd.read_csv("data/stif_indo/dev_with_pointing.csv")
-            data_dev = datasets.Dataset.from_pandas(df_dev)
-            data_dev, label_dict = prepare_data_tagging_and_pointer(data_dev, tokenizer, label_dict)
-            dev_dl = DataLoader(data_dev, batch_size=batch_size, shuffle=True, collate_fn=FelixCollator(tokenizer, pad_label_as_input=len(label_dict)))
 
-        pre_trained_bert = BertForTokenClassification.from_pretrained(model_path_or_name, num_labels=len(label_dict))
-        lit_tagger = LitTaggerOrInsertion(pre_trained_bert, lr=1e-3, num_classes=len(label_dict), class_weight=class_weights, tokenizer=tokenizer, label_dict=label_dict)
-        train_dl = DataLoader(data_train, batch_size=batch_size, shuffle=True, collate_fn=FelixCollator(tokenizer, pad_label_as_input=len(label_dict)))
-        trainer = Trainer(accelerator='mps', devices=1, val_check_interval=20, check_val_every_n_epoch=None, callbacks=[rich_cb])
-        trainer.fit(lit_tagger, train_dl, dev_dl)
+    if part == "tagger":
+        tagger(
+            df_train,
+            data_train,
+            tokenizer,
+            batch_size,
+            model_path_or_name,
+            with_validation,
+            do_compute_class_weight,
+            device,
+            label_dict,
+        )
+    elif part == "pointer":
+        pointer(
+            data_train,
+            tokenizer,
+            batch_size,
+            with_validation,
+            device,
+            label_dict,
+        )
+    elif part == "insertion":
+        processed_train_data = "data/stif_indo/train_insertion"
+        processed_dev_data = "data/stif_indo/dev_insertion"
+        insertion(
+            processed_train_data,
+            processed_dev_data,
+            tokenizer,
+            batch_size,
+            model_path_or_name,
+            device,
+            label_dict,
+        )
 
 
-if __name__ == '__main__':
-    fire.Fire(train)
+if __name__ == "__main__":
+    fire.Fire(train_stif)
--- neo_stif/lit.py
+++ neo_stif/lit.py
@@ -12,54 +12,79 @@ class LitTaggerOrInsertion(LightningModule):
     Tagger will be trained as TokenTagging
     """
 
-    def __init__(self, model, lr, num_classes=35, class_weight=None, tokenizer=None, label_dict=None) -> None:
+    def __init__(
+        self,
+        model,
+        lr,
+        num_classes=35,
+        class_weight=None,
+        tokenizer=None,
+        label_dict=None,
+    ) -> None:
         super().__init__()
         self.model = model
         self.lr = lr
         self.class_weight = class_weight
         self.ce_loss = CrossEntropyLoss(class_weight)
-        self.num_labels=num_classes
-        self.val_f1 = F1Score('multiclass', average='macro', num_classes=num_classes, ignore_index=-100)
+        self.num_labels = num_classes
+        self.val_f1 = F1Score(
+            "multiclass", average="macro", num_classes=num_classes, ignore_index=-100
+        )
         self.tokenizer = tokenizer
-        self.label_dict = {j:i for i,j in label_dict.items()}
+        self.label_dict = {j: i for i, j in label_dict.items()}
 
     def forward(self, *args, **kwargs):
         return self.model(*args, **kwargs)
 
     def training_step(self, batch, batch_idx):
-        input_to_model = {k: v for k, v in batch.items() if k in ['input_ids', 'attention_mask', 'token_type_ids']}
+        input_to_model = {
+            k: v
+            for k, v in batch.items()
+            if k in ["input_ids", "attention_mask", "token_type_ids"]
+        }
         tag_pred = self(**input_to_model)
-        labels=batch['tag_labels']
+        labels = batch["tag_labels"]
         loss = self.ce_loss(tag_pred.logits.view(-1, self.num_labels), labels.view(-1))
         self.log("train_loss", loss, prog_bar=True)
         return loss
 
     def validation_step(self, batch, batch_idx):
-        input_to_model = {k: v for k, v in batch.items() if k in ['input_ids', 'attention_mask', 'token_type_ids']}
-        tag_pred = self(**input_to_model, labels=batch['tag_labels'])
+        input_to_model = {
+            k: v
+            for k, v in batch.items()
+            if k in ["input_ids", "attention_mask", "token_type_ids"]
+        }
+        tag_pred = self(**input_to_model, labels=batch["tag_labels"])
         loss = tag_pred.loss
-        
+
         if batch_idx == 0:
             if self.tokenizer is not None and self.label_dict is not None:
-                input_ids = input_to_model['input_ids'][0]
-                label = batch['tag_labels'][0]
-                # get tokenizer inverse vocab
-                vocab_reverse = {v: k for k, v in self.tokenizer.vocab.items()}
-                input_ids_decoded = [vocab_reverse[x.cpu().item()] for x in input_ids]
-                gold_label = [self.label_dict[z] if z != -100 else 'IGNORED' for z in label.cpu().numpy() ]
-                print(f"Input before going to output: {list(zip(input_ids_decoded, gold_label))}")
+                input_ids = input_to_model["input_ids"][0]
+                label = batch["tag_labels"][0]
+                input_ids_decoded = [self.tokenizer.vocab[x] for x in input_ids]
+                gold_label = [
+                    self.label_dict[z] if z != -100 else "IGNORED"
+                    for z in label.cpu().numpy()
+                ]
+                print(
+                    f"Input before going to output: {list(zip(input_ids_decoded, gold_label))}"
+                )
                 pred = tag_pred.logits[0].argmax(-1).detach().cpu().numpy()
                 pred_label = [self.label_dict[z] for z in pred]
-                print(f"Input, pred: {list(zip(input_ids_decoded, pred_label))}")
+                print(f"Input: {input_ids_decoded} \n Pred: {pred_label}")
 
-        self.val_f1(tag_pred.logits.argmax(-1), batch['tag_labels'])
+        self.val_f1(tag_pred.logits.argmax(-1), batch["tag_labels"])
         self.log("f1_val", self.val_f1, on_epoch=True, prog_bar=True)
         self.log("val_loss", loss, prog_bar=True)
         return loss
 
     def test_step(self, batch, batch_idx):
-        input_to_model = {k: v for k, v in batch.items() if k in ['input_ids', 'attention_mask', 'token_type_ids']}
-        tag_pred = self(**input_to_model, labels=batch['tag_labels'])
+        input_to_model = {
+            k: v
+            for k, v in batch.items()
+            if k in ["input_ids", "attention_mask", "token_type_ids"]
+        }
+        tag_pred = self(**input_to_model, labels=batch["tag_labels"])
         loss = tag_pred.loss
         self.log("test_loss", loss, prog_bar=True)
         return loss
@@ -74,30 +99,42 @@ class LitPointer(LightningModule):
     Class Training Pointers
     """
 
-    def __init__(self, config, lr, num_classes) -> None:
+    def __init__(self, config, lr, num_classes=None) -> None:
         super().__init__()
         self.model = PointerNetwork(config)
         self.lr = lr
-        
+
     def forward(self, *args, **kwargs):
         return self.model(*args, **kwargs)
 
     def training_step(self, batch, batch_idx):
-        input_to_model = {k: v for k, v in batch.items() if k in ['input_ids', 'attention_mask', 'token_type_ids']}
-        tag_pred = self(**input_to_model, labels=batch['tag_labels'])
+        input_to_model = {
+            k: v
+            for k, v in batch.items()
+            if k in ["input_ids", "attention_mask", "token_type_ids"]
+        }
+        tag_pred = self(**input_to_model, labels=batch["point_labels"])
         loss = tag_pred.loss
         return loss
 
     def validation_step(self, batch, batch_idx):
-        input_to_model = {k: v for k, v in batch.items() if k in ['input_ids', 'attention_mask', 'token_type_ids']}
-        tag_pred = self(**input_to_model, labels=batch['tag_labels'])
+        input_to_model = {
+            k: v
+            for k, v in batch.items()
+            if k in ["input_ids", "attention_mask", "token_type_ids"]
+        }
+        tag_pred = self(**input_to_model, labels=batch["point_labels"])
         loss = tag_pred.loss
         self.log("val_loss", loss, prog_bar=True)
         return loss
 
     def test_step(self, batch, batch_idx):
-        input_to_model = {k: v for k, v in batch.items() if k in ['input_ids', 'attention_mask', 'token_type_ids']}
-        tag_pred = self(**input_to_model, labels=batch['tag_labels'])
+        input_to_model = {
+            k: v
+            for k, v in batch.items()
+            if k in ["input_ids", "attention_mask", "token_type_ids"]
+        }
+        tag_pred = self(**input_to_model, labels=batch["point_labels"])
         loss = tag_pred.loss
         self.log("test_loss", loss, prog_bar=True)
         return loss
